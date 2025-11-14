5b81835e796d639b4a2d946ddfccacaa4ddf3bba
unnihari2332-cmd <unnihari2332@gmail.com>
2025-11-14 16:25:19 +0530

    Update SwathiKondePR.tsx

--- src/pages/portfolio/SwathiKondePR.tsx
+++ src/pages/portfolio/SwathiKondePR.tsx
@@ -16,7 +16,7 @@ import {
   Hash,
 } from "lucide-react";
 
-const heroBg = "/0003.jpg"; // hero image from public
+const heroBg = "/0002.jpg"; // hero image from public
 
 /** ================================
  *  YouTube Auto-Scroller (full 16:9)
